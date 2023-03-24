0ff945586974e3dc87f6f54e28c32473c1c4e952
Nikhil <nk9599094257@gmail.com>
2023-03-24 17:55:48 +0530

    24-03-2025 second update

--- app.js
+++ app.js
@@ -12,8 +12,21 @@ const productRoutes = require("./api/routes/product.js");
 const orderRouter = require("./api/routes/order.js");
 
 app.use(morgan("dev"));
-app.use(bodyParser.urlencoded({extended : false}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With, Content-Type , Accept, Authorization"
+  );
+
+  if (req.method === "OPTIONS") {
+    res.header('Acess-Control-Allow-Methods', 'PUT, PATCH , DELET , POST , GET')
+    return res.status(200).json({})
+  }
+});
 
 app.use("/products", productRoutes);
 
