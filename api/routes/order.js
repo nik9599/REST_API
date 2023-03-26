6fbdbda1044c5722f1930ac2931cb55f5ba3fc8a
Nikhil <nk9599094257@gmail.com>
2023-03-27 00:38:39 +0530

    27-03-2023

--- api/routes/order.js
+++ api/routes/order.js
@@ -1,36 +1,111 @@
-const express = require('express')
+const express = require("express");
+const mongoose = require("mongoose");
+const Order = require("../models/orders.js");
+const request = require('postman-request')
 
 const router = express.Router();
 
-router.get('/',(req , res , next)=>{
-    res.status(200).json({
-        message : 'order get request'
+router.get("/", (req, res, next) => {
+  Order.find()
+    .select(" _id , product , quantity")
+    .then((result) => {
+      res.status(200).json({
+        total_Order: result.length,
+        order: result.map((doc) => {
+          return {
+            _id: doc._id,
+            product_id: doc.product,
+            Quantity: doc.quantity,
+            request: {
+              type: "GET",
+              Order_url: "http://localhost:3000/order/" + doc._id,
+              product_url: "http://localhost:3000/products/" + doc.product,
+            },
+          };
+        }),
+      });
     })
+    .catch((error) => {
+      res.status(500).json({
+        message: error,
+      });
+    });
+});
+
+router.post("/", (req, res, next) => {
+  const order = new Order({
+    _id: new mongoose.Types.ObjectId(),
+    quantity: req.body.quantity,
+    product: req.body.productid,
+  });
+
+  order
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        message: "Order placed succesfully",
+        placedOrder: {
+          id: result._id,
+          product_Id: result.product,
+          Quantity: result.quantity,
+        },
+        response: {
+          url: "http://localhost:3000/products/" + result.product,
+          type: "POST",
+        },
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({ message: error });
+    });
+});
+
+router.get("/:orderId", (req, res, next) => {
+  const id = req.params.orderId;
+
+  Order.findById(id)
+    .then((result) => {
+      if (result) {
+        res.status(200).json({
+          _id: result._id,
+          product_id: result.product,
+          Quantity:  result.quantity,
+          request: {
+            type: "GET",
+            Order_url: "http://localhost:3000/order/" + result._id,
+            product_url: "http://localhost:3000/products/" + result.product,
+          },
+        });
+      }else{
+        res.status(404).json({
+          message :'NOT FOUND'
+        })
+      }
+    })
+    .catch();
+});
+
+router.delete("/:orderId", (req, res, next) => {
+  const id = req.params.orderId;
+   const url = 'http://localhost:3000/order/'+id
+   request({url : url , json :true}, (err , {body})=>{
+          if(body._id == id){
+            Order.findByIdAndDelete(id).then(result =>{
+              res.status(200).json({
+                message : 'sucesfully deleted'
+              })
+            }).catch(err =>{
+              res.status(500).json({
+                error : err
+              })
+            })
+          }else{
+            res.status(404).json({
+              error : "order id not found"
+          })
+   }
+ 
+});
 })
 
-router.post('/',(req , res , next)=>{
-    const order = {
-        name : req.body.productId,
-        quantity :  req.body.quantity
-    }
-    res.status(201).json({
-        message : 'order post request',
-        order : order
-    })
-})
-
-router.get('/:ordertId' , (req , res, next)=>{
-    res.status(200).json({
-        message : 'order request'
-    })
-})
-
-router.delete('/:ordertId' , (req , res, next)=>{
-    res.status(200).json({
-        message : 'order delete request',
-        id : req.params.ordertId
-    })
-})
-
-
-module.exports = router
+module.exports = router;
--- api/routes/products.js
+++ api/routes/products.js
@@ -7,13 +7,25 @@ const Product = require("../models/product.js");
 
 router.get("/", (req, res, next) => {
   Product.find()
+    .select("_id, name , price")
     .exec()
     .then((docs) => {
-      if (docs.length >= 0) {
-        res.status(200).json(docs);
-      } else {
-        res.status(200).json({ message: "Empty data" });
-      }
+      const response = {
+        number_of_entries: docs.length,
+        product: docs.map((doc) => {
+          return {
+            name: doc.name,
+            Price: doc.price,
+            id: doc._id,
+            request: {
+              type: "GET",
+              url: "http://localhost:3000/products/" + doc._id
+            },
+          };
+        }),
+      };
+
+      res.status(200).json(response);
     })
     .catch((err) => {
       res.status(500).json({
@@ -32,22 +44,38 @@ router.post("/", (req, res, next) => {
     .save()
     .then((result) => {
       res.status(200).json({
-        message: "hello post request",
-        createdProduct: product,
+        message: "Created succesfully",
+        createdProduct: {
+          name : result.name,
+          price : result. price,
+          id : result._id,
+          response :{
+            type : 'POST',            
+            url : 'http://localhost:3000/products/'+result._id
+          }
+        }
       });
     })
-    .catch((err) => console.log(err));
+    .catch((error) => console.log(error));
 });
 
 router.get("/:productId", (req, res, next) => {
   const id = req.params.productId;
 
   Product.findById(id)
+  .select('name , price , _ id ')
     .exec()
     .then((doc) => {
       if (doc) {
         res.status(200).json({
-          doc,
+            name :doc.name,
+            price : doc.price,
+            id : doc._id,
+            response :{
+              type:'GET',
+              url :'http://localhost:3000/products/'+doc._id
+            }
+          
         });
       } else {
         res.status(404).json({
@@ -64,18 +92,23 @@ router.get("/:productId", (req, res, next) => {
 
 router.patch("/:productId", (req, res, next) => {
   const id = req.params.productId;
-  const updateOps ={}
+  const updateOps = {};
 
-  for(const ops of req.body){
-    updateOps[ops.propName] = ops.value
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
   }
-  Product.findByIdAndUpdate(
-    { _id: id },
-    { $set: updateOps  }
-  )
+  Product.findByIdAndUpdate({ _id: id }, { $set: updateOps })
     .exec()
     .then((result) => {
-      res.status(200).json(result);
+      res.status(200).json({
+        name : result.name,
+        price : result.price,
+        id : result._id,
+        response: {
+            type : 'PATCH',
+            url : 'http://localhost:3000/products/'+result._id
+        }
+      });
     })
     .catch((err) => {
       res.status(500).json({
@@ -90,7 +123,9 @@ router.delete("/:productId", (req, res, next) => {
   Product.findByIdAndDelete({ _id: id })
     .exec()
     .then((result) => {
-      res.status(200).json(result);
+      res.status(200).json({
+        message :"Deleted succesfully"
+      });
     })
     .catch((err) => {
       res.status(500).json(err);
